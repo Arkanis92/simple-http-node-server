57efaeb3dbc4b503a9273130e487a4e6f58820ee
Victor Kung <vkung15@gmail.com>
2019-03-20 13:09:43 -0400

    Fixes 1

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
+const http = require('http');
+const port = 3000;
 
-fs.writeFile('hello-world.txt', 'Hello to this great world.', 'utf8', (err) => {
-  if (err) throw err;
+const requestHandler = (request, response) => {
+  fs.appendFile('hello-world.txt', 'Hello to this greate world!', utf8, err => {
+    if (err) throw err;
+    console.log('Saved');
+  });
+};
 
-  console.log('success')
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error: ${err}`);
+  }
+  console.log(`server is listening on ${port}`);
 });
